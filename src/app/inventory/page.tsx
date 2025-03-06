15b23f3bdd5cb39dc122bf28ed8512ef352a80e2
SaiSawant1 <saisawant2003@gmail.com>
2025-03-07 04:55:04 +0530

    date picker range

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,8 +1,17 @@
+import { DatePickerRange } from "@/components/date-range-picker";
+import { Button } from "@/components/ui/button";
+
 export default function InventoryPage() {
   return (
     <div className="px-10 py-4">
-      <div className="flex">
+      <div className="flex justify-between items-center">
         <h1 className="font-bold text-5xl">Dashboard</h1>
+        <div className="flex items-center gap-4">
+          <DatePickerRange className="dark" />
+          <Button size={"lg"} variant={"secondary"}>
+            Download
+          </Button>
+        </div>
       </div>
     </div>
   );
